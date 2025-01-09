dbf570b9cc09f35b24d1884877b4e3329095d38e
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-10 02:36:30 +0530

    created databse model

--- Django/chai/models.py
+++ Django/chai/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPES_CHOICE = [
+        ('ML', "MASALA"),
+        ('GR', "GINGER"),
+        ('KL', "KIWI"),
+        ('PL', "PLAIN"),
+        ("EL", "ELACHI")
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    added_date = models.DateTimeField(default=timezone.now)
+    types = models.CharField(max_length=2, choices=CHAI_TYPES_CHOICE)
+    description = models.TextField(default='')
+    price = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.name
+
+
--- Django/chai/views.py
+++ Django/chai/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import ChaiVariety
 
 # Create your views here.
 
 def all_chai(request):
-    return render(request, 'all_chai.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'all_chai.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    print(request)
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(request, 'chai_detail.html', {'chai':chai})
