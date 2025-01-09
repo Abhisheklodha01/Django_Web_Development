8881b13642775233febb5dcee8949e274a99fd2e
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-09 17:23:19 +0530

    added multiple route

--- Django/Django/views.py
+++ Django/Django/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def home(request):
     #return HttpResponse("Hello World")
     return render(request, 'website/index.html')
+
+def about(request):
+    #return HttpResponse("Hello World")
+    return render(request, 'website/index.html')
+
+def contact(request):
+    #return HttpResponse("Hello World")
+    return render(request, 'website/index.html')
