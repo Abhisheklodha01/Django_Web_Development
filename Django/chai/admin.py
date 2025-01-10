c080570aad244f5861a4dcc389d30c1b3dfc3c2f
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-10 18:32:48 +0530

    relationship between models in Django

--- Django/chai/admin.py
+++ Django/chai/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiCertificate, ChaiReviews, ChaiStore
 
 # Register your models here.
-admin.site.register(ChaiVariety)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReviews
+    extra = 2
+
+class ChaiVerietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'types', 'added_date',)
+    inlines = [ChaiReviewInline]
+
+class ChaiStoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)      
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certficate_number', 'issue_date',)    
+
+admin.site.register(ChaiVariety, ChaiVerietyAdmin)
+admin.site.register(ChaiStore, ChaiStoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
+
