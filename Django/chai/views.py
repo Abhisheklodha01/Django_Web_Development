6b3aa2d1ac2ed39246d2fe6d87885d3e73d725f7
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-10 19:40:30 +0530

    forms in Django

--- Django/chai/views.py
+++ Django/chai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiStore
+from .forms import ChaiVarityForm
 
 # Create your views here.
 
@@ -9,6 +10,18 @@ def all_chai(request):
     return render(request, 'all_chai.html', {'chais': chais})
 
 def chai_detail(request, chai_id):
-    print(request)
     chai = get_object_or_404(ChaiVariety, pk=chai_id)
     return render(request, 'chai_detail.html', {'chai':chai})
+
+def chai_stores_view(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST)
+        if form.is_valid():
+           chai_variety = form.cleaned_data['chai_varity']
+           stores = ChaiStore.objects.filter(chai_varieties=chai_variety)
+    else:
+        form = ChaiVarityForm()       
+
+    return render(request, 'chai_stores.html', {'stores' : stores, 'form' : form})
+    
